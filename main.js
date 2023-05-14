45c75681e5fcd083fb694825e714adb6d6cd3821
Luizgsmkw <luizgustavokw@gmail.com>
2023-05-14 13:19:20 -0300

    projeto aluramidi

---
+++ main.js
@@ -0,0 +1,36 @@
+function tocaSom(cont) {
+  const elemento = document.querySelector(cont);
+  if(elemento === null || elemento === undefined || elemento.localName !== 'audio'){
+    console.log('Elemento não possui valor inserido, ou valor inserido não é valido')
+  } else {
+    return elemento.play()
+  }
+  
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (var i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+
+  const instrumento = tecla.classList[1];
+
+  const idSom = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idSom);
+  };
+
+  tecla.onkeydown = function (e) {
+    if(e.code === 'Space' || e.code === 'Enter'){
+        tecla.classList.add('ativa')
+    }
+  }
+
+  tecla.onkeyup = function (e) {
+    if(e.code === 'Space' || e.code === 'Enter'){
+        tecla.classList.remove('ativa') 
+    }
+    
+  }
+}
